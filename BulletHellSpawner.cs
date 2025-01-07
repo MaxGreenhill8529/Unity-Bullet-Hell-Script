e508d55c99d050928b8dc5ec698663f7892a4c48
MaxGreenhill8529 <tidy-scorer-stem@duck.com>
2025-01-06 17:50:36 -0800

    Update BulletHellSpawner.cs

--- BulletHellSpawner.cs
+++ BulletHellSpawner.cs
@@ -2,45 +2,67 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BulletHellSpawner : MonoBehaviour
 {
+    //INSTRUCTIONS
+    //Create parent objects to hold the spawn locations for the projectiles.
+    //Create parent objects to hold the sprites you want to enable to telegraph to your players that an attack is incoming.
+    //Add them to the corresponding fields in the attackPatternsArray.
+    //The functions you will need to call are "AddToCue" and "RunQueue", in that order.
+    //"AddToCue" when you wish to telegraph your attack.
+    //"RunQueue" when you wish to execute your attack.
+
+    //EXPLANATION
+    //The reason im using this parent structure is because it helps keep the hierarchy more organized and the script more flexible.
+    //Allowing you to simple switch a single parent object instead of dozens of individual transforms and spriteRenderers.
+
     [Header("Required References")]
+    [Tooltip("The gameObject to spawn.")]
     [SerializeField] private GameObject projectile;
+    [Tooltip("A list of where to spawn the projectiles")]
     [SerializeField] private AttackPattern[] attackPatterns;
     [Header("Optional Settings")]
+    [Tooltip("The parent to spawn the projectiles under in the hierarchy.")]
     [SerializeField] private Transform parent;
+    [Tooltip("If you don't want repeating attacks, leave as false.")]
+    [SerializeField] private bool repeats = false;
 
-
-
-    private List<AttackPattern> queue = new();
+    //Records the last Attack Pattern spawned in order to prevent repeats
     private int lastIndex;
+    private List<AttackPattern> queue = new();
+
+    #region Public Methods
     public void AddToQueue()
     {
+        //Fixes a spawning error!!!
+        //More testing is needed to add functionality for spawning multiple attack patterns at once.
         if (queue.Count >= 1)
         {
             return;
         }
+
         lastIndex = ReturnNewNumber(0, attackPatterns.Length, lastIndex);
         queue.Add(attackPatterns[lastIndex]);
         ShowTelegraphLinesUsingParents(queue[queue.Count - 1]);
     }
-
-    public int ReturnNewNumber(int minValue, int maxValue, int lastValue)
-    {
-        int randomValue = Random.Range(minValue, maxValue);
-        while (randomValue == lastValue)
-        {
-            randomValue = Random.Range(minValue, maxValue);
-        }
-        return randomValue;
-    }
-
 
     public void RunQueue()
     {
         Spawn(queue[queue.Count - 1]);
         queue.RemoveAt(queue.Count - 1);
     }
+    #endregion
 
-    public void Spawn(AttackPattern attackPattern)
+    #region Private Methods (You can poke around if you like, but not neccesary ;) )
+    private int ReturnNewNumber(int minValue, int maxValue, int lastValue)
+    {
+        int randomValue = Random.Range(minValue, maxValue);
+        while (repeats == false && randomValue == lastValue)
+        {
+            randomValue = Random.Range(minValue, maxValue);
+        }
+        return randomValue;
+    }
+
+    private void Spawn(AttackPattern attackPattern)
     {
         foreach (GameObject parent in attackPattern.spawnParents)
         {
@@ -52,10 +74,10 @@ public class BulletHellSpawner : MonoBehaviour
                 Instantiate(projectile, transform.position, transform.rotation, this.parent);
             }
         }
-        HideTelegraphLinesUsingParents(attackPattern);
+        HideTelegraph(attackPattern);
     }
 
-    public void ShowTelegraphLinesUsingParents(AttackPattern attackPattern)
+    private void ShowTelegraph(AttackPattern attackPattern)
     {
         foreach (GameObject parent in attackPattern.telegraphParents)
         {
@@ -68,7 +90,7 @@ public class BulletHellSpawner : MonoBehaviour
     }
 
 
-    public void HideTelegraphLinesUsingParents(AttackPattern attackPattern)
+    private void HideTelegraph(AttackPattern attackPattern)
     {
         foreach (GameObject parent in attackPattern.telegraphParents)
         {
@@ -79,11 +101,16 @@ public class BulletHellSpawner : MonoBehaviour
             }
         }
     }
+    #endregion
 }
+
 [System.Serializable]
 public class AttackPattern
 {
+    [Tooltip("Name of the Attack Pattern (Used only for your own convenience, has no impact on the code)")]
     public string name;
+    [Tooltip("A list of parent object(s) that have sprites as children (Used as telegraphers for the projectiles)")]
     public GameObject[] telegraphParents;
+    [Tooltip("A list of parent object(s) that have transform components in the children (Used as spawn location for the projectiles)")]
     public GameObject[] spawnParents;
 }
